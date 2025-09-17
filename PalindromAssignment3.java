522cb373cd9dbc8a1552e8ce03015f23f5358545
shafeenam16-art <shafeenam16@gmail.com>
2025-09-17 13:02:47 +0530

    Add files via upload

--- PalindromAssignment3.java
+++ PalindromAssignment3.java
@@ -2,23 +2,23 @@ package week1.day2140925;
 
 public class PalindromAssignment3 {
 
-	 public static void main(String[] args) {
-	        int number = 12345;  // you can change the number to test
-	        int original = number;
-	        int reversed = 0;
+	 
+		 public static void main(String[] args) {
+		        int input = 12345;   // You can change the number to test
+		        int output = 0;    // To store the reversed number
+		        int rem;           // To store remainder
 
-	        // Reverse the number
-	        while (number > 0) {
-	            int digit = number % 10;
-	            reversed = (reversed * 10) + digit;
-	            number = number / 10;
-	        }
+		        // Reverse the number using for loop
+		        for (int i = input; i > 0; i = i / 10) {
+		            rem = i % 10;                  // get last digit
+		            output = (output * 10) + rem;  // build reversed number
+		        }
 
-	        // Check palindrome
-	        if (original == reversed) {
-	            System.out.println(original + " is a Palindrome.");
-	        } else {
-	            System.out.println(original + " is not a Palindrome.");
-	        }
-	    }
+		        // Check if palindrome
+		        if (input == output) {
+		            System.out.println(input + " is a Palindrome.");
+		        } else {
+		            System.out.println(input + " is NOT a Palindrome.");
+		        }
+		    }
 }
